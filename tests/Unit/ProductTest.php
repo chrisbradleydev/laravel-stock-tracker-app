4f0763d493bcf75ebdb209b193aac193fb68eabc
Chris Bradley <chris_bradley@sweetwater.com>
2021-03-23 23:00:13 -0400

    rename example test

--- tests/Unit/ProductTest.php
+++ tests/Unit/ProductTest.php
@@ -8,7 +8,7 @@ use App\Models\Stock;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class ExampleTest extends TestCase
+class ProductTest extends TestCase
 {
     use RefreshDatabase;
 
