ba637e9e6a6f0914b67eda22d90c14c0a31b6500
Chris Bradley <chris_bradley@sweetwater.com>
2021-03-23 22:58:55 -0400

    move example test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,18 +1,35 @@
 <?php
 
-namespace Tests\Unit;
+namespace Tests\Feature;
 
-use PHPUnit\Framework\TestCase;
+use App\Models\Product;
+use App\Models\Retailer;
+use App\Models\Stock;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
+    use RefreshDatabase;
+
+    /** @test */
+    public function check_stock_for_products_at_retailers()
     {
-        $this->assertTrue(true);
+        $switch = Product::create(['name' => 'Nintendo Switch']);
+
+        $bestBuy = Retailer::create(['name' => 'Best Buy']);
+
+        $this->assertFalse($switch->inStock());
+
+        $stock = new Stock([
+            'price'=> 1000,
+            'url'=> 'http://foo.com',
+            'sku'=> '12345',
+            'in_stock' => true
+        ]);
+
+        $bestBuy->addStock($switch, $stock);
+
+        $this->assertTrue($switch->inStock());
     }
 }
